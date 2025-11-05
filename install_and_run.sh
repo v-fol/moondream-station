50d01cee31655fb43c04ce8831c53a4435bce11e
Volodymyr Foliush <flenderinfo@gmail.com>
2025-11-05 10:46:43 +0200

    fix

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Setting pod to terminate after 12 hours..."
+nohup bash -c "sleep 12h; runpodctl remove pod $RUNPOD_POD_ID" &>/dev/null &
+
 echo "Cloning moondream-station..."
 git clone https://github.com/v-fol/moondream-station.git
 cd moondream-station
