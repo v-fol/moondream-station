b8dc0e7e803aa255b2c3b2db2fb5723ab05f800f
Volodymyr Foliush <flenderinfo@gmail.com>
2025-11-11 16:14:43 +0100

    change start script

--- install_and_run.sh
+++ install_and_run.sh
@@ -4,15 +4,11 @@ set -e
 echo "Setting pod to terminate after 12 hours..."
 nohup bash -c "sleep 12h; runpodctl remove pod $RUNPOD_POD_ID" &>/dev/null &
 
-echo "Cloning moondream-station..."
-git clone https://github.com/v-fol/moondream-station.git
-cd moondream-station
-
 echo "Installing main requirements..."
-pip install -r requirements.txt
+pip install -r /workspace/moondream-station/requirements.txt
 
 echo "Installing backend requirements..."
-pip install -r backends/mds_backend_0/requirements.txt
+pip install -r /workspace/moondream-station/backends/mds_backend_0/requirements.txt
 
 echo "Starting API server..."
-python3 api_launcher.py
+python3 /workspace/moondream-station/api_launcher.py
